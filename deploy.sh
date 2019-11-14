b8e38fd6bb1e9396ad8a80b46ec14ae4626ba5d5
przemykk <nakon94@gmail.com>
2019-11-14 23:47:24 +0100

    chaning dockers

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker build -t przemyyk/multi-client:latest -t przemyyk/multi-client:#SHA -f ./client/Dockerfile ./client 
-docker build -t przemyyk/multi-server:latest -t przemyyk/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t przemyyk/multi-worker:latest -t przemyyk/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+# docker build -t przemyyk/multi-client:latest -t przemyyk/multi-client:#SHA -f ./client/Dockerfile ./client 
+# docker build -t przemyyk/multi-server:latest -t przemyyk/multi-server:$SHA -f ./server/Dockerfile ./server
+# docker build -t przemyyk/multi-worker:latest -t przemyyk/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push przemyyk/multi-client:latest
-docker push przemyyk/multi-server:latest
-docker push przemyyk/multi-worker:latest
+# docker push przemyyk/multi-client:latest
+# docker push przemyyk/multi-server:latest
+# docker push przemyyk/multi-worker:latest
 
-docker push przemyyk/multi-client:$SHA
-docker push przemyyk/multi-server:$SHA
-docker push przemyyk/multi-worker:$SHA
+# docker push przemyyk/multi-client:$SHA
+# docker push przemyyk/multi-server:$SHA
+# docker push przemyyk/multi-worker:$SHA
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
